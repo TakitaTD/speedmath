bb377137b1fd210c2092e157b4532c7c54cdd81a
Tim Takita <takitatd@gmail.com>
2022-10-15 18:41:23 +1100

    package upgrade

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test';
 
-test('index page has expected h1', async ({ page }) => {
+test('Difficulty exists', async ({ page }) => {
 	await page.goto('/');
 	expect(await page.textContent('label.svelte-146l4qm')).toBe('Difficulty');
 });
