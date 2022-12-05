a44f952aaa46d0e9e5cb36657aa945f234f61d66
TakitaTD <takitatd@gmail.com>
2022-12-05 12:28:53 +1100

    nuke repo

--- tests/test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('Difficulty exists', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('label.svelte-146l4qm')).toBe('Difficulty');
-});
